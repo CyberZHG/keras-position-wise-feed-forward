75de08adaec30969b4abcbc7784bc98d0f97fbae
CyberZHG <853842+CyberZHG@users.noreply.github.com>
2019-06-15 11:37:27 +0800

    Add dropout for hidden units

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 pycodestyle --max-line-length=120 keras_position_wise_feed_forward tests && \
-    nosetests --with-coverage --cover-html --cover-html-dir=htmlcov --cover-package=keras_position_wise_feed_forward tests
+    nosetests --nocapture --with-coverage --cover-erase --cover-html --cover-html-dir=htmlcov --cover-package=keras_position_wise_feed_forward --with-doctest
